1ad791832048edd41462d01c14a5c918f46a02ad
Pradas137 <pradas137@gmail.com>
2020-02-21 20:23:36 +0100

    adrin

--- web/index.php
+++ web/index.php
@@ -7,7 +7,12 @@
 	<h1>Hakantor</h1>
 	<h2>File</h2>
 <?php
-	phpinfo();
+
+	$dbopts = parse_url(getenv('DATABASE_URL'));
+
+	$dbconn = pg_connect($dbopts)
+	or die('No se ha podido conectar: ' . pg_last_error());
 	?>
+
 </body>
 </html>
