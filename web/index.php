5f3e7d58163a32db32aa38ef7186ef5793c58acd
Pradas137 <pradas137@gmail.com>
2020-02-28 19:17:04 +0100

    heroku

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 	<h1>Hakantor</h1>
-	<h2>File</h2>
+	<h2>Conection</h2>
 <?php
 
 	$dbopts = getenv('DATABASE_URL');
@@ -15,7 +15,20 @@
 
 	echo "Connectado"
 
+	if(isset($_POST["Usuari"]))
+
 	?>
+
+ <form action="verifica_usuario.php" method="POST" id="formLogin">
+            
+            <label>User</label><br/>
+            <input name="user" type="text" id="user" /><br/>
+            <label>Password</label><br/>
+            <input name="password" type="password" id="password" /><br/><br/>
+            
+            <input type="submit" name="Submit" value="LOGIN" /> 
+
+</form>
 
 </body>
 </html>
