1e94efd883c3f6ad759733c4d2a891ccb14aa2a3
Pradas137 <pradas137@gmail.com>
2020-02-28 19:28:57 +0100

    heroku

--- web/index.php
+++ web/index.php
@@ -15,22 +15,6 @@
 
 	echo "Connectado"
 
-	$query = 'SELECT * FROM usuari';
-	$result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
-	echo "<table>\n";
-	while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-    echo "\t<tr>\n";
-    foreach ($line as $col_value) {
-        echo "\t\t<td>$col_value</td>\n";
-    }
-    echo "\t</tr>\n";
-}
-echo "</table>\n";
-
-pg_free_result($result);
-
-pg_close($dbopts);
-
 	?>
 
 </body>
