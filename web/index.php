ca19d4b425570ff19631dcc1f813655a4be6fbd5
Pradas137 <pradas137@gmail.com>
2020-02-28 19:39:16 +0100

    heroku

--- web/index.php
+++ web/index.php
@@ -17,5 +17,12 @@
 
 	?>
 
+<form action="index.php" method="POST" id="formLogin">
+	<label>User</label><br/>
+	<input name="user" type="text" id="user" /><br/>
+	<label>Password</label><br/>
+	<input name="password" type="password" id="password" /><br/><br/>
+	<input type="submit" name="Submit" value="LOGIN" /> 
+</form>
 </body>
 </html>
