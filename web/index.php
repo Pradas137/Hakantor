216961c155474d7dfa32948e7b01ccd3e4d86d14
Pradas137 <pradas137@gmail.com>
2020-02-28 18:45:22 +0100

    HakantorCambio

--- web/index.php
+++ web/index.php
@@ -8,10 +8,13 @@
 	<h2>File</h2>
 <?php
 
-	$dbopts = parse_url(getenv('DATABASE_URL'));
+	$dbopts = getenv('DATABASE_URL');
 
 	$dbconn = pg_connect($dbopts)
 	or die('No se ha podido conectar: ' . pg_last_error());
+
+	echo "Connectado"
+
 	?>
 
 </body>
